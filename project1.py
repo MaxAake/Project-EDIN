1571ae433ec42bac89b05e0132fa3bcf8aa60471
Max <maxgustafsson.ugglum@gmail.com>
2022-11-17 10:37:34 +0100

    Wörk Döne

--- project1.py
+++ project1.py
@@ -1,37 +1,66 @@
 
-from math import floor, sqrt
+from math import floor, sqrt, gcd
+import subprocess
 
 
 N = 89692892892645583511288289
-
+Nsmall = 114450059
 def findR(N, L): 
-    F = findPrimes(L)
-    smoothPrimes = []
-    for prime in F:
-        if prime < L-5:
-            smoothPrimes.append(prime)
-    print(F)
+    F = findPrimes(L + 5)
     triedNums = set()
+    computedNums = []
     factorList = []
+    rModList = []
     j = 1
     k = 1
     while(len(triedNums) < L - 5):
         r = floor(sqrt(k*N) + j)
-        rMod = r%N
-        [works, factors] = factor(rMod, smoothPrimes)
-        if(works & (rMod not in triedNums)):
-            triedNums.add(rMod)
-            factorList.append(factors)
-            print(len(factorList))
+        rMod = (r**2)%N
+        [works, factors] = factor(rMod, F)
+        computedNum = 1
+        for fac in factors:
+            computedNum = computedNum * fac
+        if(works == True and (computedNum not in triedNums)):
+            triedNums.add(computedNum)
+            computedNums.append(computedNum)
+            factorList.append([r, factors])
+            rModList.append(rMod)
         j += 1
         if(j == 100):
             k += 1
             j = 0
-    print(factorList)
+    file1 = open("MyFile.txt", "w")
+    file1.write(str(L-5) + " " + str(L+5) + "\n")
+    for factors in factorList:
+        string = ""
+        counter = 0
+        nextprime = factors[1][0]
+        for prime in F:
+            if prime == nextprime and counter < len(factors[1]):
+                string += "1 "
+                counter += 1
+                if counter < len(factors[1]):
+                    nextprime = factors[1][counter]
+            else:
+                string += "0 "
+        string += "\n"
+        file1.write(string)
+    subprocess.call(["GaussBin.exe", "MyFile.txt", "outFile.txt"])
+    file2 = open("outFile.txt", "r")
+    lines = file2.readlines()
+    for line in lines[1 : len(lines)]:
+        characters = line.split()
+        prodX = 1
+        prodY = 1
+        for i in range(0, len(characters)-1):
+            if characters[i] == "1":
+                prodY = prodY * computedNums[i]
+                prodX = prodX * rModList[i]
+        tryFactor = gcd(prodY - prodX, N)
+        if tryFactor != 1:
+            print("Possible factor: " + str(tryFactor) + "\n Other factor would be: " + str(N/tryFactor) + " remainder: " + str(N%tryFactor))
+            break
         
-            
-            
-
 def factor(rMod, smoothPrimes):
     factors = []
     for prime in smoothPrimes:
@@ -41,7 +70,7 @@ def factor(rMod, smoothPrimes):
             temp += 1
         if(temp % 2 == 1):
             factors.append(prime)
-    if(rMod != 1):
+    if(rMod != 1 or len(factors) < 2):
         return [False, []]
     else:
         return [True, factors]
@@ -59,4 +88,4 @@ def findPrimes(L):
         i += 2
     return primes
     
-findR(N, 1000)
+findR(testNum, 1000)
