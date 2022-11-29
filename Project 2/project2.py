469835c7b773f03b99a1cf05b34541a01b9bf936
Max <maxgustafsson.ugglum@gmail.com>
2022-11-29 22:16:21 +0100

    solved

--- Project 2/project2.py
+++ Project 2/project2.py
@@ -7,9 +7,9 @@ F5 = [2, 0, 2, 1]
 F2 = [0, 0, 1, 1] # x^4 + x + 1
 
 resultString = ""
-fiveString = ""
-working5 = [0, 0, 0, 1]
-working2 = [0, 0, 0, 1]
+twoString = ""
+working5 = [0, 0, 0, 0]
+working2 = [0, 0, 0, 0]
 
 def coolF(numFromFive, numFromTwo):
     return numFromFive *2 + numFromTwo 
@@ -26,8 +26,8 @@ for _ in range(10003):
     newNumber = coolF(taken5, taken2)
     resultString += str(newNumber)
     if working2 == [0, 0, 0, 0]:
-        working2 = [1, 1, 0, 1]
-    elif working2 == [1, 1, 1, 1]: 
+        working2 = [0, 0, 0, 1]
+    elif working2 == [1, 0, 0, 1]: 
         working2 = [0, 0, 0, 0]
     else:
         working2 = cycle(working2)
@@ -35,10 +35,10 @@ for _ in range(10003):
             working2[i] += taken2 * F2[i]
             working2[i] = working2[i]%2
     if working5 == [0, 0, 0, 0]:
-        working5 = [0, 1, 0, 2]
+        working5 = [0, 0, 0, 1]
         # 2 1 2 3 for 2x^4+2x^3+2x+1
         # 3 1 2 3 for x^4 + 2*x^3 + 0*x^2 + x + 3
-    elif working5 == [1, 1, 1, 1]:
+    elif working5 == [3, 3, 0, 3]:
         working5 = [0, 0, 0, 0]
     else:
         working5 = cycle(working5)
@@ -46,13 +46,13 @@ for _ in range(10003):
         for i in range(4):
             working5[i] += (taken5 * F5[i]) * 2  #times three as it is the multiplicative inverse of 2 (the coefficient of x^4)
             working5[i] = working5[i]%5
-    print(working5)
-    fiveString += str(taken5)
+    print(working2)
+    twoString += str(taken2)
 
 printFile = open("./printFile.txt", "w")
 printFile.write(resultString)
-printFile = open("./fuckaround.txt", "w")
-printFile.write(fiveString)
+printFile = open("./fuckaround2.txt", "w")
+printFile.write(twoString)
 print(resultString)
 
 
