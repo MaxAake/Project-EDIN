d16b502b64677d073af58414957c00a7ae6d33c7
Max <maxgustafsson.ugglum@gmail.com>
2022-12-08 11:10:05 +0100

    We donzo boyos, let's fucking gooooooooooooooooooooooooooooooooooooooooo

--- Project 3/project3.py
+++ Project 3/project3.py
@@ -2,12 +2,12 @@ from numpy import *
 import matplotlib.pyplot as plt
 
 #LFSR sequence polynomials C(D) for the three different LFSRs:
-# C1 = D^13 + D^11 + D^10 + D^7 + D^6 + D^4 + D^2 + D + 1
-C1 = [1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1] 
-# C2 = D^15 + D^13 + D^11 + D^10 + D^7 + D^6 + D^4 + D^2 + 1
-C2 = [1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0]
-# C3 = D^17 + D^16 + D^13 + D^10 + D^8 + D^5 + D^4 + D^2 + 1
-C3 = [1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0]
+# C13 = D^13 + D^11 + D^10 + D^7 + D^6 + D^4 + D^2 + D + 1
+C13 = [1, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1]
+# C15 = D^15 + D^13 + D^11 + D^10 + D^7 + D^6 + D^4 + D^2 + 1
+C15 = [0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+# C17 = D^17 + D^16 + D^13 + D^10 + D^8 + D^5 + D^4 + D^2 + 1
+C17 = [0, 1, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1] 
 # The sequence group 02 was given was:
 trueSeq = "0111101011011110011101000010111011001110110101101110001000101110110111101011001011110000111011110110001000010110010111111011110011000001101111100100011110111001110011000010100011001110010110111"
 
@@ -23,9 +23,8 @@ def GetSequence(poly, initialState):
             state[len(state) - i] = state[len(state) - i - 1]
         newNum = newNum % 2
         state[0] = newNum
-    print(resultString)
+    ##print(resultString)
     return resultString
-
 
 # It is assumed here that seq1 and seq2 are of the same length, otherwise throws out of bounds. 
 # Gets the similarity of two sequences.
@@ -39,12 +38,14 @@ def similarity(seq1, seq2):
 
 def testSequences(C, length):
     seq = []
+    seqs = []
     for i in range(length):
         seq += [0]
     peace = []
     for i in range(2**length):
         seq1 = GetSequence(C, seq)
         peace += [similarity(seq1, trueSeq)]
+        seqs += [seq1]
         seq[length-1] += 1
         for i in range(length):
             if seq[length-1-i] == 2:
@@ -53,22 +54,56 @@ def testSequences(C, length):
                 seq[length-1-i] = 0
             else:
                 break
-    return peace
+    return [peace, seqs]
 
 
-#P1 = testSequences(C1, 13)
-#P2 = testSequences(C2, 15)
-P3 = testSequences(C3, 17)
+P13 = testSequences(C13, 13)
+P15 = testSequences(C15, 15)
+P17 = testSequences(C17, 17)
+maxVal3 = 0
+maxSeq3 = []
 
-P = P3
-maxVal = 0
-for i in range(len(P)):
-    P[i] = abs(P[i]-0.5)
-    if P[i] > maxVal:
-        maxVal = P[i]
-#print(maxVal)
-print(P)
-#print(P2)
-#print(P3)
-plt.plot(P)
+# Returns the index of the list element with the largest deviation from 0.5.
+def getHighestIndex(listOfSequences):
+    highestDeviation = 0
+    for i in range(len(listOfSequences[0])):
+        val = listOfSequences[0][i]
+        val = abs(val - 0.5)
+        if(val > highestDeviation):
+            highestDeviation = val
+            index = i
+    return index
+
+maxSeq1 = P13[1][getHighestIndex(P13)]
+maxSeq2 = P15[1][getHighestIndex(P15)]
+maxSeq3 = P17[1][getHighestIndex(P17)]
+
+def majorityFunction(s, t, r):
+    resultstring = ""
+    for i in range(len(s)):
+        ones = 0
+        if s[i] == "1":
+            ones += 1
+        if t[i] == "1":
+            ones += 1
+        if r[i] == "1":
+            ones += 1
+        if ones > 1:
+            resultstring += "1"
+        else:
+            resultstring += "0"
+    return resultstring
+print(maxSeq1)
+print(maxSeq2)
+print(maxSeq3)
+endResult = majorityFunction(maxSeq1, maxSeq2, maxSeq3)
+#print(P13)
+#print(P15)
+print(endResult)
+print(trueSeq)
+plt.plot(P13[0])
+plt.show()
+plt.plot(P15[0])
+plt.show()
+plt.plot(P17[0])
 plt.show()
